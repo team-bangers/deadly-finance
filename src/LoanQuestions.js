79da969b0947400a3389ad2ce48ba759f70143c4
Matthew-Burfield <m1.burfield@gmail.com>
2017-05-20 20:42:12 +1000

    Converted LoanQuestions to class with state

--- src/LoanQuestions.js
+++ src/LoanQuestions.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const LoanQuestions = ({ questions }) => {
-  return (
-    <div>
-      {questions.map(question => <div>{question}</div>)}
-    </div>
-  );
+class LoanQuestions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionNumber: 1,
+    }
+  }
+
+  render() {
+    const questions = this.props.questions
+    return (
+      <div>
+        {questions.map(question => <div>{question}</div>)}
+      </div>
+    );
+  }
 }
 
 export default LoanQuestions;
