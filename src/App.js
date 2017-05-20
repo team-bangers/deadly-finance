2d77e13eebca4f5b21be13e7696b58afcd947a4b
Daniel Tait <dajtait87@gmail.com>
2017-05-20 19:48:40 +1000

    Made header responsive and added a bootstrap grid.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import header from './img/fnf-header.png';
 import './App.css';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -13,6 +14,13 @@ class App extends Component {
         <p className="App-intro">
           Get some help decoding all the financial mumbo-jumbo!
         </p>
+        <Grid>
+          <Row className="show-grid">
+            <Col md={4} xs={6}>Car Loans</Col>
+            <Col md={4} xs={6}>Home Loans</Col>
+            <Col md={4} xs={6}>Credit Cards</Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
