a1d2e15c47e830f27f08bed814e0869d1a656761
Daniel Tait <dajtait87@gmail.com>
2017-05-20 19:18:32 +1000

    Updated intro paragraph.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Are you being screwed?</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Get some help decoding all the financial mumbo-jumbo!
         </p>
       </div>
     );
