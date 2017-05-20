ce6f5dc2c584f823476a2c6d4bdd40d038ed5553
Matthew-Burfield <m1.burfield@gmail.com>
2017-05-20 20:59:51 +1000

    Added form to LoanQuestions

--- src/LoanQuestions.js
+++ src/LoanQuestions.js
@@ -1,19 +1,49 @@
 import React from 'react'
+import { FormGroup, FormControl, HelpBlock, ControlLabel } from 'react-bootstrap'
 
 class LoanQuestions extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       questionNumber: 1,
+      value: '',
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit() {
+    this.props.handleSubmit();
+  }
+
+  handleChange(e) {
+    this.setState({
+      value: e.target.value,
+    })
+  }
+
+  getValidationState() {
   }
 
   render() {
     const questions = this.props.questions
     return (
-      <div>
-        {questions.map(question => <div>{question}</div>)}
-      </div>
+      <form>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>{questions[this.state.questionNumber]}</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.value}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock>Validation is based on string length.</HelpBlock>
+        </FormGroup>
+      </form>
     );
   }
 }
